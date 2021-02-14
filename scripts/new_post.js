4ec1ebbffcaf32bb68a6183fa8090c9b5b69c703
Dorian Karter <dkarter@users.noreply.github.com>
2021-02-14 00:14:24 -0600

    Add script for creating a new blog post

---
+++ scripts/new_post.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const slugify = require('slug');
+const dateFns = require('date-fns');
+
+const title = process.argv[2];
+if (!title) {
+  throw new Error('a title is required!');
+}
+const slug = slugify(title.toLowerCase());
+const date = dateFns.format(new Date(), 'yyyy-MM-dd');
+const dir = `./content/blog/${slug}`;
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+} else {
+  throw new Error('That post already exists!');
+}
+
+const filename = `${dir}/index.mdx`;
+fs.writeFileSync(
+  filename,
+  `---
+slug: ${slug}
+date: ${date}
+title: "${title}"
+published: false
+---`,
+  err => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      return;
+    }
+    // eslint-disable-next-line no-console
+    console.log(`${title} was created!`);
+  }
+);
